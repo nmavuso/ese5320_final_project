bfe32914f1519022e1a007d3a9b3e7f7eeaf0294
aabdel0181 <ahmedabdellahhoco@gmail.com>
2024-11-01 12:37:28 -0400

    updated header

--- baseline/cmd.h
+++ baseline/cmd.h
@@ -1,15 +1,12 @@
-// chunk_deduplication.h
 #ifndef CHUNK_DEDUPLICATION_H
 #define CHUNK_DEDUPLICATION_H
 
 #include <stdint.h>
 
+// Constants
 #define NUM_CHUNKS 1000
 #define MAX_CHUNK_SIZE 8192
 #define HASH_TABLE_SIZE 1000
-
-// Function to compute a simple hash by computing addition modulo 2^64
-uint64_t compute_hash(char *chunk, int size);
 
 // Hash table entry struct
 typedef struct HashEntry
@@ -27,17 +24,11 @@ typedef struct
     int size;
 } HashTable;
 
-// Function to initialize the hash table
+// Function prototypes
+uint64_t compute_hash(char *chunk, int size);
+void insert_hash_table(HashTable *table, uint64_t key, int **value, int size);
+int **lookup_hash_table(HashTable *table, uint64_t key, int *size);
 HashTable *initialize_hash_table();
-
-// Function to deduplicate chunks
-// Returns NULL upon no match found
-int *deduplicate_chunks(uint8_t chunks[NUM_CHUNKS][MAX_CHUNK_SIZE], int chunk_sizes[NUM_CHUNKS], HashTable *hash_table);
-
-// Function to lookup in the hash table
-HashEntry *lookup_hash_table(HashTable *hash_table, uint64_t key);
-
-// Function to insert into the hash table
-void insert_hash_table(HashTable *hash_table, uint64_t key, int *value, int size);
+void deduplicate_chunks(char chunks[NUM_CHUNKS][MAX_CHUNK_SIZE], int chunk_sizes[NUM_CHUNKS], HashTable *hash_table);
 
 #endif // CHUNK_DEDUPLICATION_H
