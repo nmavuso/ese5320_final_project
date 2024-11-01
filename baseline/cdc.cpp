3f4176128ce69070dbfa6823dc76312e5b9cf766
nmavuso <nmavuso@seas.upenn.edu>
2024-10-31 23:29:46 -0400

    input files

--- baseline/cdc.cpp
+++ baseline/cdc.cpp
@@ -11,6 +11,7 @@
 
 uint64_t hash_func(unsigned char *input, unsigned int pos)
 {
+    // put your hash function implementation here
     uint64_t hash = 0;
     for (int i = 0; i < WIN_SIZE; ++i) {
         int input_char = input[pos + WIN_SIZE - i];
@@ -21,13 +22,69 @@ uint64_t hash_func(unsigned char *input, unsigned int pos)
     return hash;
 }
 
+//void cdc(unsigned char *buff, unsigned int buff_size)
+//{
+    // put your cdc implementation here
+  //  for (unsigned int i = WIN_SIZE; i < (buff_size - WIN_SIZE); i = i + 2) {
+  //      int hash = hash_func(buff, i);
+     //   if (((hash % MODULUS)) == TARGET) {
+       //     std::cout<<i + 1 <<std::endl;
+       // }
+       // int char_1 = buff[i];
+       // int char_2 = buff[i + WIN_SIZE];
+       // int next_hash = hash*PRIME - char_1*pow(PRIME, WIN_SIZE + 1) + char_2*PRIME;
+       // if (((next_hash % MODULUS)) == TARGET) {
+        //    std::cout<<i + 2<<std::endl;
+       // }
+
+
+//    }
+//void cdc(unsigned char *buff, unsigned int buff_size)
+//{
+    // put your cdc implementation here
+  //  for (unsigned int i = WIN_SIZE; i < (buff_size - WIN_SIZE); i = i + 2) {
+    //    int hash = hash_func(buff, i);
+      //  if (((hash % MODULUS)) == TARGET) {
+        //    std::cout<<i + 1 <<std::endl;
+        //}
+        //int char_1 = buff[i];
+        //int char_2 = buff[i + WIN_SIZE];
+        //int power_1 = pow(PRIME, WIN_SIZE + 1);
+        //int next_hash = hash*PRIME - char_1*power_1 + char_2*PRIME;
+        //if (((next_hash % MODULUS)) == TARGET) {
+      //      std::cout<<i + 2<<std::endl;
+    //    }
+
+
+  //
+//}
+
+//int update_hash (unsigned char *buff, unsigned int i, int hash) {
+    //int char_1 = buff[i]*pow(PRIME, WIN_SIZE + 1);
+    //int char_2 = buff[i + WIN_SIZE]*PRIME;
+    //int next_hash = hash*PRIME - char_1* + char_2;
+
+  //  return next_hash;
+//}
+
+uint64_t update_hash (unsigned char *buff, unsigned int i, uint64_t hash) {
+    int char_1 = buff[i];
+    int char_2 = buff[i + WIN_SIZE];
+    uint64_t next_hash = hash*PRIME - char_1*pow(PRIME, WIN_SIZE + 1) + char_2 *PRIME;
+
+    return next_hash;
+}
 void cdc(unsigned char *buff, unsigned int buff_size)
 {
+    // put your cdc implementation here
+    uint64_t hash = hash_func(buff, WIN_SIZE);
+    for (unsigned int i = WIN_SIZE + 1; i < (buff_size - WIN_SIZE) + 1; i++) {
 
-    for (unsigned int i = WIN_SIZE; i < (buff_size - WIN_SIZE); ++i) {
-        if (((hash_func(buff, i) % MODULUS)) == TARGET) {
-            std::cout<<i + 1<<std::endl;
+        if (((hash % MODULUS)) == TARGET) {
+            std::cout<<i <<std::endl;
         }
+        hash = update_hash(buff, i, hash);
+
     }
 
 }
